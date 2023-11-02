1d5b6d02e77a4c05de288eba159859954fc726f1
Rudy Rodarte <shadyrudy@hotmail.com>
2023-11-02 13:58:50 -0500

    CH4 - Adding load balancer alb from Terraform module examples.

--- main.tf
+++ main.tf
@@ -45,6 +45,43 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_vpc.security_group_id
+  
+  listeners = {
+    http_tcp_listeners = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Example"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
